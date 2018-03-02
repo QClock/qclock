7ebb4c39377c671ab44a660f1801fd0277f5b27b
szabolcsit <szabolcsit@hu.ibm.com>
2018-03-02 17:21:18 +0100

    update usb lib, debug

--- src/neopixels/index.js
+++ src/neopixels/index.js
@@ -1,42 +1,37 @@
 import EventEmitter from 'events'
 import log from '../log'
 
-
-
 export default class NeoPixels extends EventEmitter {
 
-    constructor () {
-        super()
+	constructor () {
+		super()
 
-        this.ready = false
+		this.ready = false
 
-        if (process.env.TESSEL) {
-            const FadeCandy = require('node-fadecandy')
-            this.fadecandy = new FadeCandy()
-            this.fadecandy.on(FadeCandy.events.READY, (...args) => {
-                log.info("FadeCandy ready")
-                //fadecandy.config.set(fadecandy.Configuration.schema.DISABLE_KEYFRAME_INTERPOLATION, 1)
-                this.fadecandy.clut.create()
-            })
-            this.fadecandy.on(FadeCandy.events.COLOR_LUT_READY, (...args) => {
+		const FadeCandy = require('node-fadecandy')
+		this.fadecandy = new FadeCandy()
+		this.fadecandy.on(FadeCandy.events.READY, (...args) => {
+			log.info("FadeCandy ready")
+			//fadecandy.config.set(fadecandy.Configuration.schema.DISABLE_KEYFRAME_INTERPOLATION, 1)
+			this.fadecandy.clut.create()
+		})
+		this.fadecandy.on(FadeCandy.events.COLOR_LUT_READY, (...args) => {
+			log.info('LUT READY')
+			this.ready = true
+		})
+	}
 
-                log.info('LUT READY')
-                this.ready = true
-            })
-        }
-    }
+	send (data) {
+		if (this.ready && this.fadecandy) {
+			this.fadecandy.send(data)
+		}
+	}
 
-    send (data) {
-        if (this.ready && this.fadecandy) {
-            this.fadecandy.send(data)
-        }
-    }
-
-    clear () {
-        let data = new Uint8Array((config.OUTER + config.INNER + 6) * 3);
-        data.fill(0)
-        this.send(data)
-    }
+	clear () {
+		let data = new Uint8Array((config.OUTER + config.INNER + 6) * 3);
+		data.fill(0)
+		this.send(data)
+	}
 
 
 }
