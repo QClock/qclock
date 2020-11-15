061ec55dd363dafaa979eaddff395a7f1b7ac395
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2020-11-15 19:50:33 +0100

    update deps

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,4 +2,4 @@
 
 # todo: the "@qclock/client-aurora" should come from package.json
 mkdir -p build/web
-cp -Rf node_modules/@qclock/client-aurora/build/* build/web
+# cp -Rf node_modules/@qclock/client-aurora/build/* build/web
--- src/config.js
+++ src/config.js
@@ -14,9 +14,7 @@ module.exports = {
 
     USE_ADMIN: true,
     USE_NETWORK: true,
-    SKIP_FADECANDY: skip_facedandy,
 
-    TESSEL: tessel,
     SERVER_PORT: port,
 
     INTERVAL: 1000,
@@ -28,21 +26,8 @@ module.exports = {
     OUTER: 58,
     INNER: 56,
 
-    //OUTER_COLOR: [126,60,216], // lila RGB
-    //INNER_COLOR: [255,152,0], // sarga RGB
-
     OUTER_COLOR: [255,0,228], // pink RGB
-    INNER_COLOR: [53,225,40], //  zold RGB
-
-    //OUTER_COLOR: [255,138,0], // narancs RGB
-    //INNER_COLOR: [0,26,225], //  kek RGB
-
-    //OUTER_COLOR: [10,255,0], // zold RGB
-    //INNER_COLOR: [255,247,0], // sarga RGB
-
-
-    //OUTER_COLOR: [225,0,53], // piros RGB
-    //INNER_COLOR: [166,212,255], // 'ezust' RGB
+    INNER_COLOR: [53,225,40], //  zold RGBGB
 
     NONE_COLOR: [0,0,0],
 
@@ -52,8 +37,6 @@ module.exports = {
     DIM_LEVEL: 0.1,
 
     DIM_PREVIEW: false,
-
-    NETWORK: packagejson.qclock.network,
 
     CLOCKFACE: {}
 }
