47d73a99d907af8f092fcd757a803ec5aac3a34b
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2020-12-21 19:11:10 +0100

    fix state websocket

--- src/lib/hsl2int.js
+++ src/lib/hsl2int.js
@@ -1,6 +1,8 @@
 import hsl2rgb from './hsl2rgb'
+import log from '../log'
 
 export default function hsl2int (h, s, l) {
     const [ r, g, b ] = hsl2rgb(h,s,l)
+    log.info('r g b', [ r, g, b ])
     return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
